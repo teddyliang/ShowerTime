61e5c976856d7cadb134189735727b06c4e207a9
teddyliang <teddytduoliang@gmail.com>
2016-07-20 15:45:46 -0700

    another one

--- TimerViewController.swift
+++ TimerViewController.swift
@@ -7,9 +7,76 @@
 //
 
 import UIKit
+import MZTimerLabel
 
 class TimerViewController: UIViewController {
     
+    @IBOutlet weak var timer: MZTimerLabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let timer_ = MZTimerLabel(label: timer, andTimerType:MZTimerLabelTypeStopWatch)
+        timer_.timeFormat = "mm:ss"
+        self.stopButton.enabled = false
+        stopButton.setTitleColor(UIColor.grayColor(), forState: .Normal)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func startPauseResumeStopwatch(sender: AnyObject) {
+        if timer.counting {
+            timer.pause()
+            startButton.setTitle("Resume", forState: .Normal)
+        }
+        else if startButton.titleForState(.Normal) == "Reset"{
+            timer.reset()
+            startButton.setTitle("Start", forState: .Normal)
+            stopButton.setTitle("Stop", forState: .Normal)
+            self.stopButton.enabled = false
+            stopButton.setTitleColor(UIColor.grayColor(), forState: .Normal)
+        }
+        else {
+            timer.start()
+            startButton.setTitle("Pause", forState: .Normal)
+            self.stopButton.enabled = true
+            stopButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+            
+        }
+    }
+    @IBAction func stopStopwatch(sender: AnyObject) {
+        
+        if timer.counting {
+            stopButton.setTitle("Save", forState: .Normal)
+            startButton.setTitle("Reset", forState: .Normal)
+            timer.pause()
+        }
+        else if stopButton.titleForState(.Normal) == "Save"{
+            //Add to an array
+            stopButton.setTitle("Stop", forState: .Normal)
+            startButton.setTitle("Start", forState: .Normal)
+            timer.reset()
+        }
+        else if stopButton.titleForState(.Normal) == "Stop" && startButton.titleForState(.Normal) == "Resume"{
+            startButton.setTitle("Reset", forState: .Normal)
+            stopButton.setTitle("Save", forState: .Normal)
+        }
+        else
+        {
+            stopButton.setTitleColor(UIColor.grayColor(), forState: .Normal)
+        }
+    }
+    @IBAction func minusFive(sender: AnyObject) {
+        timer.addTimeCountedByTime(-5)
+    }
+    @IBAction func plusFive(sender: AnyObject) {
+        timer.addTimeCountedByTime(5)
+    }
+    
+    
+    
 }
